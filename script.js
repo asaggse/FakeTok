8fc9d246e805a7d2ca01b8dd5d31f2bcbede0796
Alessandro Saggese <asaggese@gmail.com>
2025-04-09 18:59:00 +0200

    Fix audio tag formatting and update video attributes in index.html; add initial script.js file

---
+++ script.js
@@ -0,0 +1,6 @@
+// Elementi del DOM: app container, i video ecc...
+const appContainerEl = document.querySelector('#app-container');
+
+// Variabili globali: è il volume attivo
+
+// Eventi: allo scroll dell'app container, al click dell'icona del volume
