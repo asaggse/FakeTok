a29a564e49d7df4dd5a10444330da33cbc07367e
Alessandro Saggese <asaggese@gmail.com>
2025-04-09 19:04:19 +0200

    Add scroll event listener to app container for logging

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ const appContainerEl = document.querySelector('#app-container');
 // Variabili globali: è il volume attivo
 
 // Eventi: allo scroll dell'app container, al click dell'icona del volume
+appContainerEl.addEventListener('scroll', function () {
+    console.log("Scroll dell'app container");
+});
